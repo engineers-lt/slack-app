af04e799bd7d1b559a819dc2571d9788a995c57f
mottox2 <y.mottox2@gmail.com>
2018-11-13 22:52:49 +0900

    Post message when reaction_added is detected

--- src/events.js
+++ src/events.js
@@ -1,10 +1,18 @@
+import { WebClient } from '@slack/client'
+
 exports.handler = async (event, context, callback) => {
   const body = JSON.parse(event.body)
   const slackEvent = body.event
   console.log(JSON.stringify(body, null, 4))
 
   if (slackEvent && slackEvent.type === 'reaction_added') {
-    console.log(JSON.stringify(slackEvnet, null, 4))
+    const web = new WebClient(process.env.SLACK_TOKEN)
+    const res = await web.chat.postMessage({
+      channel: 'times_mottox2',
+      text: `reaction_added: ${slackEvent.reaction}
+channel: ${slackEvent.channel}
+`,
+    })
   }
 
   callback(null, {

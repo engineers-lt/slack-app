6dc2ed41d7debcc2fe9e4c20c0634326594deac3
mottox2 <y.mottox2@gmail.com>
2019-02-03 21:15:20 +0900

    Add channels command

--- src/commands.js
+++ src/commands.js
@@ -5,8 +5,24 @@ exports.handler = async (event, context, callback) => {
   const query = qs.parse(event.body)
   console.log(query)
 
-  const response = {
-    text: "text"
+  const [command, ...args] = query.text.split(' ')
+
+  let response = {
+    text: "Command is not found. usage: /kotori channels"
+  }
+
+  if (!command) {
+    response = {
+      text: "Command is not found. usage: /kotori channels"
+    }
+  }
+
+  if (command === 'channels') {
+    const webClient = new WebClient(process.env.SLACK_TOKEN)
+    const result = await webClient.channels.list()
+    response = {
+      text: result.channels.map(c => c.name).join('\n')
+    }
   }
 
   return callback(null, {
